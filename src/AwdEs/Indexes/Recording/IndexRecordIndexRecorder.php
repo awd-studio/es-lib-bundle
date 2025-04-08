c9471fa0e2c32211256fb4c0ed5038f6fd29c11c
Anton Karpov <awd.com.ua@gmail.com>
2025-04-08 12:32:54 +0300

    Implemented recording logic in IndexRecordIndexRecorder.
    Added a constructor to inject IndexRecordFactory and EntityManagerInterface. Updated the record() method to build a record, persist it, and flush changes to the database.

--- src/AwdEs/Indexes/Recording/IndexRecordIndexRecorder.php
+++ src/AwdEs/Indexes/Recording/IndexRecordIndexRecorder.php
@@ -5,14 +5,24 @@ declare(strict_types=1);
 namespace AwdEs\EsLibBundle\AwdEs\Indexes\Recording;
 
 use Awd\ValueObject\IDateTime;
+use AwdEs\EsLibBundle\AwdEs\Indexes\Record\Factory\IndexRecordFactory;
 use AwdEs\Indexes\Index;
 use AwdEs\Indexes\Recording\IndexRecorder;
+use Doctrine\ORM\EntityManagerInterface;
 
 final readonly class IndexRecordIndexRecorder implements IndexRecorder
 {
+    public function __construct(
+        private IndexRecordFactory $factory,
+        private EntityManagerInterface $em,
+    ) {}
+
     #[\Override]
     public function record(Index $index, IDateTime $recordedAt): void
     {
-        // TODO: Implement record() method.
+        $record = $this->factory->build($index, $recordedAt);
+
+        $this->em->persist($record);
+        $this->em->flush();
     }
 }
